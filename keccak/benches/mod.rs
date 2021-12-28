277ec3579f8d317f352d273090d982d87984ccb3
Alexander Wagner <alexander.wagner@ulal.de>
2021-12-28 12:49:26 +0100

    Add generic keccak-p and keccak-f {200, 400, 800}

--- keccak/benches/mod.rs
+++ keccak/benches/mod.rs
@@ -3,6 +3,24 @@ extern crate keccak;
 extern crate test;
 
 #[bench]
+fn f200(b: &mut test::Bencher) {
+    let mut data = [0u8; 25];
+    b.iter(|| keccak::f200(&mut data));
+}
+
+#[bench]
+fn f400(b: &mut test::Bencher) {
+    let mut data = [0u16; 25];
+    b.iter(|| keccak::f400(&mut data));
+}
+
+#[bench]
+fn f800(b: &mut test::Bencher) {
+    let mut data = [0u32; 25];
+    b.iter(|| keccak::f800(&mut data));
+}
+
+#[bench]
 fn f1600(b: &mut test::Bencher) {
     let mut data = [0u64; 25];
     b.iter(|| keccak::f1600(&mut data));
