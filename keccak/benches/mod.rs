469432672bc9c8f69cf5c98d642552b873fbf1d7
Alexander Wagner <alexander.wagner@ulal.de>
2022-01-13 22:52:01 +0100

    Add f1600x{2, 4, 8}

--- keccak/benches/mod.rs
+++ keccak/benches/mod.rs
@@ -2,26 +2,28 @@
 extern crate keccak;
 extern crate test;
 
-#[bench]
-fn f200(b: &mut test::Bencher) {
-    let mut data = [0u8; 25];
-    b.iter(|| keccak::f200(&mut data));
+use keccak::{f1600, f200, f400, f800};
+
+macro_rules! impl_bench {
+    ($name:ident, $fn:ident, $type:expr) => {
+        #[bench]
+        fn $name(b: &mut test::Bencher) {
+            let mut data = [$type; 25];
+            b.iter(|| $fn(&mut data));
+        }
+    };
 }
 
-#[bench]
-fn f400(b: &mut test::Bencher) {
-    let mut data = [0u16; 25];
-    b.iter(|| keccak::f400(&mut data));
-}
+impl_bench!(b_f200, f200, 0u8);
+impl_bench!(b_f400, f400, 0u16);
+impl_bench!(b_f800, f800, 0u32);
+impl_bench!(b_f1600, f1600, 0u64);
 
-#[bench]
-fn f800(b: &mut test::Bencher) {
-    let mut data = [0u32; 25];
-    b.iter(|| keccak::f800(&mut data));
-}
+#[cfg(feature = "simd")]
+mod simd {
+    use keccak::simd::{f1600x2, f1600x4, f1600x8, u64x2, u64x4, u64x8};
 
-#[bench]
-fn f1600(b: &mut test::Bencher) {
-    let mut data = [0u64; 25];
-    b.iter(|| keccak::f1600(&mut data));
+    impl_bench!(b_f1600x2, f1600x2, u64x2::splat(0));
+    impl_bench!(b_f1600x4, f1600x4, u64x4::splat(0));
+    impl_bench!(b_f1600x8, f1600x8, u64x8::splat(0));
 }
